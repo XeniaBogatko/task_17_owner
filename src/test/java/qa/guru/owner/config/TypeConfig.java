929394792cce361aeb1f00c68e4468dc18ccc672
Xenia Bogatko <bogatkok@gmail.com>
2022-12-12 19:25:53 +0100

    initial commit

---
+++ src/test/java/qa/guru/owner/config/TypeConfig.java
@@ -0,0 +1,26 @@
+package qa.guru.owner.config;
+
+import org.aeonbits.owner.Config;
+
+// проверить File, URL, Path, byte[]
+public interface TypeConfig extends Config {
+
+    @Key("integer")
+    Integer getInteger();
+
+    @Key("double")
+    Double getDouble();
+
+    @Key("boolean")
+    Boolean getBoolean();
+
+    @Key("enum")
+    Browser getEnum();
+
+//    @Key("url")
+//    Browser getEnum();
+//
+//    @Key("file")
+//    Browser getEnum();
+
+}
